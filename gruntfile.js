1b9ef586de9fcf7e39d8a9323289e185901a8d2e
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-14 22:05:57 +0530

    Add Plugins

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,46 @@
 
 module.exports = function (grunt) {
 
+    require('load-grunt-tasks')(grunt);
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9']
+            },
+            target: {
+                files: { 'css/styleprefixed.css': 'css/style.css' }
+            }
+        },
+        watch: {
+            files: ['css/style.css'],
+            tasks: ['autoprefixer']
+        },
+        copy: {
+            main: {
+                files: [
+                    { src: 'css/style.css', dest: 'css/stylecopied.css' }
+                ]
+            }
+        },
+        cssmin: {
+            target: {
+                files: { 'styleprefixed.min.css': ['css/styleprefixed.css'] }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'js/output.min.js': ['js/*.js']
+                }
+            }
+        }
     });
+
+    // grunt.loadNpmTasks('grunt-autoprefixer');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['copy']);
 }
---
+++ js/script.js
@@ -0,0 +1,3 @@
+console.log("this is test");
+
+console.log("this is test 2");
